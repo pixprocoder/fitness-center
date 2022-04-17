15020cb9e15da6a7b146dda1a66f588e74a9ca6c
kabir <makkobir79@gmail.com>
2022-04-17 22:29:24 +0600

    router install and route and header setup complited

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Header from "./pages/Shared/Header/Header";
 import Footer from "../src/pages/Shared/Footer/Footer";
+import Home from "../src/pages/Home/Home/Home";
+import Services from "./pages/Home/Services/Services";
+import Blogs from "../src/pages/Blogs/Blogs";
+import About from "../src/pages/About/About";
+import Contact from "../src/pages/Contact/Contact";
+import Login from "../src/pages/Register/Login/Login";
+import Register from "../src/pages/Register/Register/Register";
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
       <Footer />
     </>
   );
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      <h1>header </h1>
-    </div>
+    <>
+      <nav>
+        <div>
+          <Link to="/home">Home</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/blogs">Blogs</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+
+        <div>
+          <Link to="/Login">Login</Link>
+          <Link to="/register">Register</Link>
+        </div>
+      </nav>
+    </>
   );
 };
 
